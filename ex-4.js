06867d64e4fa03b9ce294c711e61b8bd84125cbf
Suriya <true.many.032@gmail.com>
2025-02-27 13:50:01 +0700

    fix:ex4

--- ex-4.js
+++ ex-4.js
@@ -1,16 +1,18 @@
 // Start coding here
 let calculator = {
-    add: (a, b) => {
-         return a + b; },
-    subtract: (a, b) => {
-         return a - b; },
-    multiply: (a, b) => {
-         return a * b; },
-    divide: (a, b) => {
-         return a / b; },
+    add: (a, b) => a + b ,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b ,
+    divide: (a, b) => a / b,
 };
+
+// calculator.mod = (a, b) => { return a % b;}
+
 const addResult = calculator.add(3000, 10);
 console.log(addResult);
 
 const divideResult = calculator.divide(3000, 10);
 console.log(divideResult);
+
+// console.log(calculator.mod(11,3));
+// console.log(calculator);
